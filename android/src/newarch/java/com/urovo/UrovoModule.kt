2308d3451fc7102beb3714f3fb7dfa0097e0f8a0
iliapnmrv <iliapnmrvv@gmail.com>
2025-10-03 16:17:49 +0300

    wip

--- android/src/newarch/java/com/urovo/UrovoModule.kt
+++ android/src/newarch/java/com/urovo/UrovoModule.kt
@@ -2,7 +2,7 @@ package com.urovo
 
 import com.facebook.react.bridge.*
 
-class UrovoModule(reactContext: ReactApplicationContext): NativeUrovoSpec(reactContext) {
+class UrovoModule(reactContext: ReactApplicationContext): NativeRNUrovoSpec(reactContext) {
   private var implementation: UrovoModuleImpl = UrovoModuleImpl()
 
   override fun getName(): String = UrovoModuleImpl.NAME
